5c6e0885c969aee74466210508a3126edfb9a15d
Jeff Bezanson <jeff.bezanson@gmail.com>
2018-02-10 01:57:18 -0500

    make `dims` (previously sometimes called `region`) a keyword argument

--- base/array.jl
+++ base/array.jl
@@ -2031,7 +2031,9 @@ julia> findmax([1,7,7,NaN])
 (NaN, 4)
 ```
 """
-function findmax(a)
+findmax(a) = _findmax(a, :)
+
+function _findmax(a, ::Colon)
     if isempty(a)
         throw(ArgumentError("collection must be non-empty"))
     end
@@ -2072,7 +2074,9 @@ julia> findmin([7,1,1,NaN])
 (NaN, 4)
 ```
 """
-function findmin(a)
+findmin(a) = _findmin(a, :)
+
+function _findmin(a, ::Colon)
     if isempty(a)
         throw(ArgumentError("collection must be non-empty"))
     end
